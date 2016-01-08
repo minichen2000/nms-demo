7544e1b729c5fde0a3e49bb1fc9bb871180a362f
minichen <chenmind@gmail.com>
2016-01-08 22:51:39 +0800

    ..

--- js/app.js
+++ js/app.js
@@ -3,7 +3,18 @@
 /* App Module */
 
 var nmsdemoApp = angular.module('nmsdemoApp', [
-  'nmsdemoControllers',
-  'treeDirectives',
-  'perfect_scrollbar'
+    'nmsdemoControllers',
+    'treeDirectives',
+    'perfect_scrollbar',
+    'ui.router'
 ]);
+nmsdemoApp.config(['$stateProvider', '$urlRouterProvider',
+function ($stateProvider) {
+
+    // Now set up the states
+    $stateProvider
+        .state('main', {
+            url: "/main",
+            templateUrl: "partials/main.html"
+        });
+}]);
--- js/controllers.js
+++ js/controllers.js
@@ -7,12 +7,13 @@
 var nmsdemoControllers=  angular.module('nmsdemoControllers', []);
 
 nmsdemoControllers.controller('treeController',
-function($scope) {
-	$scope.neTreeData = neData;
-	$scope.mapTreeData = mapData;
-	$scope.tlTreeData = tlData;
-	$scope.trailTreeData = trailData;
-	$scope.pathTreeData = pathData;
-	$scope.evcTreeData = evcData;
-	$scope.leftTreeChanged={changed:true};
+function() {
+    var vm=this;
+	vm.neTreeData = neData;
+	vm.mapTreeData = mapData;
+	vm.tlTreeData = tlData;
+	vm.trailTreeData = trailData;
+	vm.pathTreeData = pathData;
+	vm.evcTreeData = evcData;
+	vm.leftTreeChanged={changed:true};
 });  
